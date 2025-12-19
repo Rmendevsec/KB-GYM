f97ec96e621624c9cdb51db842bf0226e2c12920
Rmendevsec <ezrawork12@gmail.com>
2025-12-19 14:12:46 +0300

    feat: add access and refresh token secrets to .env and implement login endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,31 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 
 const PORT = 5000
+const jwt = require("jsonwebtoken")
 
-app.listen(console.log(`serveer is running on port ${PORT}`))
+app.use(express.json())
+const posts = [{
+    username: "rmen",
+    title: "First Post"
+}, {
+    username: "john",
+    title: "Hello World"
+}]
+app.get("/posts", (req, res) =>{
+    res.json(posts)
+    
+
+})
+
+app.post("/login", (req,res)=>{
+    const username = req.body.username
+    const user = {name: username}
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE)
+    res.json({accessToken: accessToken})
+
+})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
