410c0d727cc632edb61e9437fb0ca45183ce5022
Rmendevsec <ezrawork12@gmail.com>
2025-12-19 14:12:53 +0300

    fix: correct token verification logic in authenticateToken function

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const posts = [{
     username: "john",
     title: "Hello World"
 }]
-app.get("/posts", (req, res) =>{
+app.get("/posts",authenticateToken, (req, res) =>{
     res.json(posts)
     
 
@@ -22,10 +22,20 @@ app.get("/posts", (req, res) =>{
 app.post("/login", (req,res)=>{
     const username = req.body.username
     const user = {name: username}
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({accessToken: accessToken})
 
 })
+
+function authenticateToken(req,res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+    if (token == null ) return res.sendStatus(401)
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user))=>{
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+}
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
