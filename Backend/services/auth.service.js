f1b8eb36a9e1e14df3d5ce8cfc7b222d8dfc8aef
Rmendevsec <ezrawork12@gmail.com>
2025-12-20 07:52:22 +0300

    feat: implement user registration service with password hashing

---
+++ Backend/services/auth.service.js
@@ -0,0 +1,14 @@
+const {user, role, role} = require("../models")
+const passwordUtil = require("../utils/password")
+const jwtUtil = require("../utils/jwt")
+
+exports.register = async (data) =>{
+    const role = await role.findOne({where: {name: data.role || "user"}})
+    const hashedPassword = await passwordUtil.hash(data.password)
+    
+    return user.create({
+        email: data.email,
+        password: hashedPassword,
+        fullname: data.fullname
+    })
+}
