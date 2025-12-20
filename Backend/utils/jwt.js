da40138d6dea6a05ba0441d59fd0460405642974
Rmendevsec <ezrawork12@gmail.com>
2025-12-20 07:46:46 +0300

    feat: implement JWT sign and verify functions

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -1,2 +1,7 @@
 const jwt = require("jsonwebtoken")
 const {JWT_SECRETE} =require("../")
+
+exports.sign = (payload) => {
+    jwt.sign(payload, JWT_SECRETE, {expiresIn: "24h"})
+}
+exports.verify = (token) => jwt.verify(token, JWT_SECRETE)
